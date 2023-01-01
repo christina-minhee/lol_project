cfd55b42c5dc9ca94ea4c5a65c7dddefd4e45323
ChrisK007 <tinachristina0218@gmail.com>
2023-01-01 11:28:21 +0900

    modify Profile/index.css to Profile/index.sass

--- src/pages/Home/components/Body/Profile/index.js
+++ src/pages/Home/components/Body/Profile/index.js
@@ -1,30 +1,37 @@
-import "./index.css";
+import styles from "./index.module.sass";
 
 const Profile = ({ summonerData }) => {
   return (
-    <div className="profile">
-      {/* TO DO button:? list */}
-      <div className="profile_card">
+    <div className={styles.container}>
+      <div className={styles.medals}>
+        <div className={styles.medal}>
+          <span>S3&nbsp;</span>Bronze
+        </div>
+        <div className={styles.medal}>
+          <span>S3&nbsp;</span>Bronze
+        </div>
+        <div className={styles.medal}>
+          <span>S3&nbsp;</span>Bronze
+        </div>
+        <div className={styles.medal}>
+          <span>S3&nbsp;</span>Bronze
+        </div>
+      </div>
+      <div className={styles.content}>
         <img
-          className="image_border"
+          className={styles.img_border}
           src={summonerData.profileBorderImageUrl}
           alt="user_profle_img_border"
         />
-        <div className="image_container">
-          <img
-            className="image_profile"
-            src={summonerData.profileImageUrl}
-            alt="user_profile_img"
-          />
+        <div className={styles.img_container}>
+          <img src={summonerData.profileImageUrl} alt="user_profile_img" />
         </div>
-        <div className="profile_detail">
-          <p className="player_id">{summonerData.name}</p>
-          <span className="player_title">
-            레더 랭킹
-            <span className="player_ladder_ranking">
-              {summonerData.ladderRank.rank}
-            </span>
-            위 (상위 {summonerData.ladderRank.rankPercentOfTop}%)
+        <div className={styles.summoner}>
+          <p className={styles.name}>{summonerData.name}</p>
+          <span className={styles.title}>
+            레더&nbsp;랭킹&nbsp;
+            <span className={styles.rank}>{summonerData.ladderRank.rank}</span>
+            위&nbsp;(상위&nbsp;{summonerData.ladderRank.rankPercentOfTop}%)
           </span>
         </div>
       </div>

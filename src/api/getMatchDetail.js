20e0434b6e7af7a30c445134df9a5c5829a32039
ChrisK007 <tinachristina0218@gmail.com>
2023-01-04 11:38:54 +0900

    delete unneeded code

--- src/api/getMatchDetail.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export async function getMatchDetail(gameId) {
-  return axios
-    .get(
-      `https://codingtest.op.gg/api/summoner/{summonerName}/matchDetail/${gameId}`
-    )
-    .then((res) => res.json());
-}
